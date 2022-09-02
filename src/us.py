f2cfac53b422709c0b69a79dd5f2ca8b8ea1fc1f
R1DF <33981824+R1DF@users.noreply.github.com>
2022-09-02 20:50:09 +0600

    adaped ulken sharshy

--- src/us.py
+++ src/us.py
@@ -3,6 +3,6 @@ from place import Place
 
 # Ulken Sharshy class
 class UlkenSharshy(Place):
-	def __init__(self):
-		pass
+	def __init__(self, coords, master=None):
+		Place.__init__(self, coords, master)
 
