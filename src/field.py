32c9f4112c8035aa63940398e251b3609c63071a
R1DF <33981824+R1DF@users.noreply.github.com>
2022-09-02 20:49:54 +0600

    made field ten times better and finished generation

--- src/field.py
+++ src/field.py
@@ -24,4 +24,28 @@ class Field:
 			# 2D lists have this order: [column][row]
 
 	def generate_places(self):
-		pass
+		coords_data = json.load(open(getcwd() + "\\coords.json", "r"))
+
+		# Shops have collections of random coordinates per place.
+		for shop_coords_pack in coords_data["shops"]:
+			coords = choice(shop_coords_pack)
+			self.field[coords[1]][coords[0]].determine_contents("SHOP", coords)
+
+		# Houses and offices have predetermined coordinates, but they're all over the place.
+		for locations in ["houses", "offices"]:
+			for coords in coords_data[locations]:
+				self.field[coords[1]][coords[0]].determine_contents(
+					"HOUSE" if locations == "houses" else "OFFICE", coords
+				)
+
+		# There is only one hospital.
+		coords = coords_data["hospital"]
+		self.field[coords[1]][coords[0]].determine_contents("HOSPITAL", coords)
+
+		# There's only one parking spot, but its location is randomly picked.
+		parking_spot_coords = choice(coords_data["parking_spots"])
+		self.field[parking_spot_coords[1]][parking_spot_coords[0]].determine_contents("PARKING", parking_spot_coords)
+
+		# The Ulken Sharshy is technically the same place but the code has a clever way to mask it.
+		for coords in coords_data["ulken_sharshy"]:
+			self.field[coords[1]][coords[0]].determine_contents("ULKEN_SHARSHY", coords)
