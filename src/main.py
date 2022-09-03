a9504c084ae2ed75617f512ec1079aeece771798
R1DF <33981824+R1DF@users.noreply.github.com>
2022-09-03 12:04:09 +0600

    added more info to the game and made it less ugly (somewhat)

--- src/main.py
+++ src/main.py
@@ -9,6 +9,9 @@ Making this small text-based game as a CS project. Your job is simple: Get a wat
 from coltext import *
 from gameinit import Game
 
+# Preinstalled imports
+import os
+
 # Debug - write below this comment for any debugging and set the "debug" variable to True
 debug = False
 
@@ -16,16 +19,18 @@ debug = False
 if debug:
     quit()
 
+os.system("title Protests Simulator 2022")
 clear()
 print("""
-Protests Simulator 2022
-Your objective throughout this game is to get a water bottle while mass protests happen around the city.
+Protests Simulator 2022 (running v1.0.0 indefinite)
+Your objective throughout this game is to get a water bottle while mass protests happen around the city. Beware enemies and get back safely.
+This game was based on the January 2022 unrest that occurred in Kazakhstan and has been made as a Computer Science project.
 """)
 input("\nPress Enter to begin.\n")
-clear()
 
 # Making the player
 name = force_request("Enter your name: ")
+clear()
 
 # Game loop
-game = Game()
+game = Game(name)
