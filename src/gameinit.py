bd65583b78890fba26723ed4c345a1ec9b031b40
R1DF <33981824+R1DF@users.noreply.github.com>
2022-09-05 13:52:27 +0600

    added game winning conditions

--- src/gameinit.py
+++ src/gameinit.py
@@ -9,7 +9,7 @@ class Game:
 	def __init__(self, player_name):
 		# Making necessary attributes
 		self.field = Field()
-		self.player = Player(player_name, Player.make_random_coords())
+		self.player = Player(player_name[0].upper() + player_name[1:], Player.make_random_coords())
 		self.command_tracer = CommandTracer(self)
 
 		# Giving help note
@@ -18,17 +18,37 @@ class Game:
 		# Game loop
 		self.game_loop()
 
+		# Checking how the game went
+		coltext.clear()
+		if self.player.get_status() != "dead":
+			coltext.colformat("M#Congratulations!~|")
+			print("You successfully found a water bottle and took it with you alive.")
+
+			print("Final statistics:")
+			self.command_tracer.trace("me")
+			print("Well played.\n\n")
+
+		else:
+			coltext.alarm("Game over!")
+			print(f"You are dead.\nCause of death: {self.player.get_cause_of_death()}\nBetter luck next time!\n\n")
+
+		# Game ending code
+		input()
+		coltext.clear()
+		quit()
+
+
 	def game_loop(self):
-		while self.player.get_status() != "dead":
-			command = coltext.request("Enter what you'd like to do: ").strip().upper()
+		while (self.player.get_status() != "dead") and ([self.player.x, self.player.y] != self.field.parking_spot_coords): # lose and win condition
+			command = coltext.request("Enter command: ").strip().upper()
 			self.command_tracer.trace(command)
-			print("\n") # line breaker
+			print("\n")  # line breaker
 
 	def return_map(self):
 		# Formatting map and styling it
 		formatted_map = []
 		for column in self.field.field:
-			formatted_map.append(["###" if location.contents == None else location.contents.name[:3] for location in column])
+			formatted_map.append(["###" if location.contents is None else location.contents.name[:3] for location in column])
 		formatted_map[self.player.y][self.player.x] = "YOU"
 
 		for column in formatted_map:
@@ -43,5 +63,6 @@ HOS - Hospital
 PAR - Parking spot
 HOU - House
 ULK - The Ulken Sharshy"""))
+
 
 
