6e9fdf2244f5c222c8d55789f4b3ce6df905b537
R1DF <33981824+R1DF@users.noreply.github.com>
2022-09-10 16:29:17 +0600

    changed names to make the "TAKE" and "PUT DOWN" commands less ruthless

--- src/items/bread.py
+++ src/items/bread.py
@@ -4,4 +4,4 @@ from .item import Item
 # Bread class
 class Bread(Item):
     def __init__(self, holder,):
-        Item.__init__(self, holder, "BEAD", "Load of Bread")
+        Item.__init__(self, holder, "BREAD", "Bread")
--- src/items/dice_of_fate.py
+++ src/items/dice_of_fate.py
@@ -4,4 +4,4 @@ from .item import Item
 # Apple class
 class DiceOfFate(Item):
     def __init__(self, holder):
-        Item.__init__(self, holder, "DICE_OF_FATE", "Mysterious Dice of Fate")
+        Item.__init__(self, holder, "DICE_OF_FATE", "Dice of Fate")
