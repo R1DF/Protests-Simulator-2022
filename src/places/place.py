54006f9eaff0237522837566a60701200a0dc0f6
R1DF <33981824+R1DF@users.noreply.github.com>
2022-09-10 16:32:48 +0600

    "TAKE" command handled in Place class

--- src/places/place.py
+++ src/places/place.py
@@ -4,6 +4,7 @@ This class exists as a parent class for every other location type. Do not confus
 # Internal imports
 import coltext
 import random
+from items.item import Item, display_name_to_text
 from items.water_bottle import WaterBottle
 
 # Place blueprint
@@ -24,7 +25,7 @@ class Place:
 		self.determine_description()
 
 	def determine_description(self):
-		pass # Has to be individually defined
+		pass  # Has to be individually defined
 
 	def disclose(self):
 		coltext.announce(self.display_name)
@@ -32,11 +33,56 @@ class Place:
 
 	def show_contents(self):
 		if self.contents:  # PyCharm why
+			items_and_amounts = {
+				"APPLE": 0,
+				"BREAD": 0,
+				"POTATO": 0,
+				"DICE_OF_FATE": 0
+			}
+
+			for item in self.contents:
+				items_and_amounts[item.name] += 1
+
 			print(coltext.colformat(f"Contents of C#{self.name.lower()}~|:"))
-			for element in self.contents:
-				print(element.display_name)
+			for item_name in items_and_amounts:
+				if items_and_amounts[item_name] != 0:
+					print(f"{display_name_to_text[item_name]} x{items_and_amounts[item_name]}")
+
 		else:
-			print(coltext.colformat(f"You're in {self.article} C#{self.name.lower()}~|, but there's nothing here."))
+			print(coltext.colformat(f"You're in {self.article} C#{self.display_name.lower()}~|, but there's nothing here."))
+
+	def handle_take_query(self, player):
+		item = coltext.force_request("Enter the item you want to take and its amount (default is 1) (e.g. water 2): ").split()
+
+		contents_names = [x.display_name.lower() for x in self.contents]
+		# Getting item and amount from input
+		if item[-1].isnumeric():
+			amount = int(item[-1])
+			item_name = " ".join(item[:-1])
+
+		else:
+			amount = 1
+			item_name = " ".join(item)
+
+		# Checking if item exists
+		if item_name.lower() in contents_names:
+			if contents_names.count(item_name.lower()) >= amount:
+				# Taking the objects and placing them in the player's consumables bag
+				for item_object in range(amount):
+					exact_item = self.contents[contents_names.index(item_name)]
+					if exact_item.item_type == "consumable":  # checking if consumable
+						player.consumables.append(exact_item)
+					else:
+						player.inventory.append(exact_item)  # otherwise
+					self.contents.remove(exact_item)
+
+				# Notifying
+				print("Taken. Your inventory has been updated.")
+			else:
+				coltext.alarm("That's way too much...")
+		else:
+			coltext.alarm("You can't pick up something that isn't here in the first place.")
+
 
 	# def make_decisive(self): NOT YET
 	# 	self.contents.append(WaterBottle())
