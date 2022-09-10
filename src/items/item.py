274ec0478d88e5666ff7d0136299cc8ea771389a
R1DF <33981824+R1DF@users.noreply.github.com>
2022-09-10 16:30:59 +0600

    added items dictionary + status to help now and in the future

--- src/items/item.py
+++ src/items/item.py
@@ -1,3 +1,11 @@
+# For algorithms
+display_name_to_text = {
+    "APPLE": "Apple",
+    "BREAD": "Bread",
+    "POTATO": "Potato",
+    "DICE_OF_FATE": "Dice of Fate"
+}
+
 # Item blueprint
 class Item:
     def __init__(self, holder, name, display_name):
@@ -5,6 +13,7 @@ class Item:
         self.holder = holder
         self.name = name
         self.display_name = display_name
+        self.item_type = "consumable"  # original type is always consumable
 
     def use(self):
         pass
