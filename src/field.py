190fc91b7d2e8001fe6ff0e27603ac6c54cc9685
R1DF <33981824+R1DF@users.noreply.github.com>
2022-09-11 19:21:44 +0600

    randomization done (put down and take logic error)

--- src/field.py
+++ src/field.py
@@ -31,67 +31,54 @@ class Field:
 			# 2D lists have this order: [column][row]
 
 	def make_item_distributions(self):
-		self.house_and_office_items = (  # Weapons and shields spawn differently, only one per random place
-			("APPLE", 2),  # the 2 signifies how much of apples can there be
-			("BREAD", 2),
-			("POTATO", 2),
-			("DICE_OF_FATE", 1),
-			("MEDKIT", 0)
-		)
+		# Consumables
+		self.house_and_office_items = (2, 2, 2, 1, 0)
+		self.house_and_office_rng_values = (75, 75, 15, 60, 0)
 
-		self.house_and_office_rng_values = (  # out of 100
-			75,
-			75,
-			15,
-			60,
-			0
-		)
+		self.hospital_items = (1, 1, 1, 0, 2)
+		self.hospital_rng_values = (33, 33, 0, 0, 80)
 
-		self.hospital_items = (  # Weapons and shields spawn differently, only one per random place
-			("APPLE", 1),  # the 2 signifies how much of apples can there be
-			("BREAD", 1),
-			("POTATO", 0),
-			("DICE_OF_FATE", 0),
-			("MEDKIT", 2)
-		)
+		self.shop_items = (5, 5, 2, 1, 0)
+		self.shop_rng_values = (60, 60, 50, 20, 0)
 
-		self.hospital_rng_values = (  # out of 100
-			33,
-			33,
-			0,
-			0,
-			80
-		)
+		# Equipment
+		self.us_items = (1, 1, 1, 1, 1, 1)
+		self.us_rng_values = (60, 40, 20, 60, 40, 20)
 
 	def generate_places(self):
 		coords_data = json.load(open(getcwd() + "\\coords.json", "r"))
 
 		# Shops have collections of random coordinates per place.
+		self.item_dropper.set_consumables_and_rng(self.shop_items, self.shop_rng_values)
 		for shop_coords_pack in coords_data["shops"]:
 			coords = choice(shop_coords_pack)
 			self.field[coords[1]][coords[0]].determine_contents("SHOP", coords)
+			self.item_dropper.randomize_consumables(self.field[coords[1]][coords[0]].contents)
 
 		# Houses and offices have predetermined coordinates, but they're all over the place.
-		self.item_dropper.set_items_and_rng(self.house_and_office_items, self.house_and_office_rng_values)
+		self.item_dropper.set_consumables_and_rng(self.house_and_office_items, self.house_and_office_rng_values)
 		for locations in ["houses", "offices"]:
 			for coords in coords_data[locations]:
 				self.field[coords[1]][coords[0]].determine_contents(
 					"HOUSE" if locations == "houses" else "OFFICE", coords
 				)
-				self.item_dropper.randomize(self.field[coords[1]][coords[0]].contents)
+				self.item_dropper.randomize_consumables(self.field[coords[1]][coords[0]].contents)
 
 
 
 		# There is only one hospital.
 		coords = coords_data["hospital"]
 		self.field[coords[1]][coords[0]].determine_contents("HOSPITAL", coords)
-		self.item_dropper.set_items_and_rng(self.hospital_items, self.hospital_rng_values)
-		self.item_dropper.randomize(self.field[coords[1]][coords[0]].contents)
+		self.item_dropper.set_consumables_and_rng(self.hospital_items, self.hospital_rng_values)
+		self.item_dropper.randomize_consumables(self.field[coords[1]][coords[0]].contents)
 
 		# There's only one parking spot, but its location is randomly picked.
 		self.parking_spot_coords = choice(coords_data["parking_spots"])
 		self.field[self.parking_spot_coords[1]][self.parking_spot_coords[0]].determine_contents("PARKING", self.parking_spot_coords)
 
-		# The Ulken Sharshy is technically the same place but the code has a clever way to mask it.
+		# The Ulken Sharshy is technically the same place but the code has a clever way to mask it. It's the only place to have weapons and shields.
+		self.item_dropper.set_equipment_and_rng(self.us_items, self.us_rng_values)
 		for coords in coords_data["ulken_sharshy"]:
 			self.field[coords[1]][coords[0]].determine_contents("ULKEN_SHARSHY", coords)
+			self.item_dropper.randomize_equipment(self.field[coords[1]][coords[0]].contents)
+
