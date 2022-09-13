8359818ebc80b705995b33a87165a28c8ee53c52
R1DF <33981824+R1DF@users.noreply.github.com>
2022-09-13 17:47:50 +0600

    did work on shield blueprint

--- src/items/shield.py
+++ src/items/shield.py
@@ -8,4 +8,17 @@ class Shield(Item):
         Item.__init__(self, holder, name, display_name)
 
         # Additionals
+        self.item_type = "equipment"
         self.def_bonus = bonus
+        self.equipped = False
+
+    def equip(self):
+        self.holder.shield = self
+        self.holder.defense += self.def_bonus
+        self.equipped = True
+
+    def unequip(self):
+        self.holder.equipped_shield = None
+        self.holder.defense -= self.def_bonus
+        self.equipped = False
+
