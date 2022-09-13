ae7075fd550d309df041331bfdeec85985b537ff
R1DF <33981824+R1DF@users.noreply.github.com>
2022-09-13 17:47:24 +0600

    did work on weapon blueprint

--- src/items/weapon.py
+++ src/items/weapon.py
@@ -8,4 +8,17 @@ class Weapon(Item):
         Item.__init__(self, holder, name, display_name)
 
         # Additionals
+        self.item_type = "equipment"
         self.atk_bonus = bonus
+        self.equipped = False
+
+    def equip(self):
+        self.holder.equipped_weapon = self
+        self.holder.attack += self.atk_bonus
+        self.equipped = True
+
+    def unequip(self):
+        self.holder.equipped_weapon = None
+        self.holder.attack -= self.atk_bonus
+        self.equipped = False
+
