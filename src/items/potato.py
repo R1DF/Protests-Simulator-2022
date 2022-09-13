017a79f2cf8c528e819a3a4906aa181452645927
R1DF <33981824+R1DF@users.noreply.github.com>
2022-09-13 17:50:52 +0600

    another consumable

--- src/items/potato.py
+++ src/items/potato.py
@@ -1,3 +1,6 @@
+# Package import
+from random import randint
+
 # Internal import
 from .item import Item
 
@@ -5,3 +8,12 @@ from .item import Item
 class Potato(Item):
     def __init__(self, holder):
         Item.__init__(self, holder, "POTATO", "Potato")
+
+    def use(self):
+        self.holder.health += randint(7, 14)
+        print("You ate the potato.")
+
+        if randint(1, 2) == 1:
+            self.holder.attack += randint(3, 5)
+            print("You felt a bit stronger, too.")
+
