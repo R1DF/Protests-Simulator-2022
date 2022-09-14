3376387aaf7c68cab9009d4b54821503c7432863
R1DF <33981824+R1DF@users.noreply.github.com>
2022-09-14 20:56:52 +0600

    finished fight (what a champion)

--- src/fight.py
+++ src/fight.py
@@ -5,10 +5,12 @@ The Fight class is used to simulate a battle between the player and an AI-contro
 # Internal imports
 from player import Player
 from enemy import Enemy
+from config_reader import config_data
 import coltext
 
 # Package import
 import os
+import random
 
 # Fight object
 class Fight:
@@ -17,29 +19,100 @@ class Fight:
         self.player = player
         self.enemy = enemy
         self.enemy_name = self.enemy.name
+        self.escaped = False
+        self.escapable = random.randint(1, 100) <= self.player.luck
         self.commands = ["HELP", "ATK", "ATTACK", "DEF", "DEFEND", "INV", "INVENTORY", "FLEE", "RETREAT"]
+
+        # Notifying
+        print(f"You're in a fight with a {self.enemy_name.lower()}! Oh no!\n")
+        self.notify()
 
         # Fight loop
         self.fight_loop()
+        if self.enemy.health <= 0:
+            print("Your enemy puts their hands up in surrender. You get away from the scene.\nThe fight is over.")
+        else:
+            print("The fight is over.")
 
     def fight_loop(self):
-        print(f"You're in a fight with a {self.enemy_name.lower()}! Oh no!\n")
-        while self.enemy.health > 0 and self.player.health > 0:
-            self.trace()
+        while self.player.health > 0:
+            print(f"Your HP: {self.player.health}\nYour ATK: {self.player.attack}\nYour DEF: {self.player.defense}")
+            print(f"\nEnemy's HP: {self.enemy.health}\nEnemy's ATK: {self.enemy.attack}\nEnemy's DEF: {self.enemy.defense}")
+            acted = self.trace()
+            if acted:
+                if self.escaped or self.enemy.health <= 0: # Checking goes here to not wait at the wrong moment
+                    break
+                self.enemy_trace()
+                self.player.stop_defense()
+            print("\n")
+
+    def notify(self):
+        if self.enemy.equipped_weapon is None:
+            print("The (ENEMY) does not have a weapon.".replace("(ENEMY)", self.enemy_name))
+        else:
+            print("The (ENEMY) is equipped with a (WEAPON).".replace("(ENEMY)", self.enemy_name).replace("(WEAPON)",
+                                                                                                         self.enemy.equipped_weapon.display_name))
+
+        if self.enemy.equipped_shield is None:
+            print("The (ENEMY) does not have a shield.".replace("(ENEMY)", self.enemy_name))
+        else:
+            print("The (ENEMY) has a (SHIELD).".replace("(ENEMY)", self.enemy_name).replace("(SHIELD)",
+                                                                                            self.enemy.equipped_shield.display_name))
 
     def trace(self):
         command = coltext.force_request("\nWhat would you like to do? ").strip().upper()
         match command:
             case "HELP":
                 print(coltext.colformat(open(os.getcwd() + "\\txt\\fight_commands.txt", "r").read()))
+                return False
 
             case "ATK" | "ATTACK":
-                pass
+                attempt = self.player.attack_enemy(self.enemy)
+                match attempt:
+                    case "crit":
+                        print(random.choice(config_data["playerData"]["critAttackNotices"][self.player.equipped_weapon.name]).replace(
+                            "(ENEMY)", self.enemy_name))
+
+                    case "success":
+                        print(random.choice(config_data["playerData"]["attackNotices"][self.player.equipped_weapon.name]).replace(
+                            "(ENEMY)", self.enemy_name))
+
+                    case "miss":
+                        print(random.choice(config_data["playerData"]["missNotices"][self.player.equipped_weapon.name]).replace("(ENEMY)", self.enemy_name))
+
+                    case "invulnerable":
+                        print(config_data["playerData"]["failureNotices"].replace("(ENEMY)", self.enemy_name))
+
             case "DEF" | "DEFEND":
-                pass
+                self.player.defend()
+                print(config_data["playerData"]["defenseNotice"])
+
             case "USE":
-                pass
+                self.player.handle_use_query()
+
             case "FLEE" | "RETREAT":
-                pass
+                print("You try to run away...")
+                if self.escapable:
+                    print("Success! You distract the target and vanish from their sight!")
+                    self.escaped = True
+                else:
+                    print(random.choice(["... But you lose your breath as the target you flee from catches you.", "You realize that you're backed into a corner."]))
             case _:
                 coltext.alarm("Invalid command. Remember that you're still in a fight.")
+        return True
+
+    def enemy_trace(self):
+        attempt = self.enemy.attack_player()
+        match attempt:
+            case "crit":
+                print(random.choice(config_data["enemyData"]["critAttackNotices"][self.enemy.equipped_weapon.name]).replace("(ENEMY)", self.enemy_name))
+
+            case "success":
+                print(random.choice(config_data["enemyData"]["attackNotices"][self.enemy.equipped_weapon.name]).replace("(ENEMY)", self.enemy_name))
+
+            case "miss":
+                print(random.choice(config_data["enemyData"]["missNotices"][self.enemy.equipped_weapon.name]).replace("(ENEMY)", self.enemy_name))
+
+            case "invulnerable":
+                print(config_data["enemyData"]["failureNotices"].replace("(ENEMY)", self.enemy_name))
+
