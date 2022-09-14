595799aa331b3f6791cbccca7bd05ef8b581ca90
R1DF <33981824+R1DF@users.noreply.github.com>
2022-09-14 12:27:02 +0600

    made more beautiful + KeyboardInterrupt detection

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ Making this small text-based game as a CS project. Your job is simple: Get a wat
 from coltext import *
 from gameinit import Game
 
-# Preinstalled imports
+# Package imports
 import os
 
 # Debug - write below this comment for any debugging and set the "debug" variable to True
@@ -19,17 +19,24 @@ debug = False
 if debug:
     quit()
 
-os.system("title Protests Simulator 2022")
-clear()
-print("""
-Protests Simulator 2022 (running v1.0.0 indefinite)
-Your objective throughout this game is to get a water bottle while mass protests happen around the city. Beware enemies and get back safely.
-This game was based on the January 2022 unrest that occurred in Kazakhstan and has been made as a Computer Science project.
-""")
+# Try-except statement so the game doesn't close on a weird note when you hit Ctrl+C
+try:
+    os.system("title Protests Simulator 2022")
+    clear()
+    print("""\
+ Protests Simulator 2022 (running v1.0.0 indefinite)
+    Your objective throughout this game is to get a water bottle while mass protests happen around the city. Beware enemies and get back safely.
+    This game was based on the January 2022 unrest that occurred in Kazakhstan and has been made as a Computer Science project.
+    """)
 
-# Making the player
-name = force_request("Enter your name: ")
-clear()
+    # Making the player
+    name = force_request("Enter your name: ")
+    clear()
 
-# Game loop
-game = Game(name)
+    # Game loop
+    game = Game(name)
+
+except KeyboardInterrupt:
+    clear()
+    print("Game closed due to Keyboard Interrupt exception.")  # this will be turn-offable later
+
