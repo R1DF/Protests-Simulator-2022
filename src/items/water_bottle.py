72a83477a771df5c42fee2240aec6779935dcabf
R1DF <33981824+R1DF@users.noreply.github.com>
2022-09-15 17:59:54 +0600

    water bottle can actually be used now

--- src/items/water_bottle.py
+++ src/items/water_bottle.py
@@ -5,3 +5,19 @@ from .item import Item
 class WaterBottle(Item):
     def __init__(self, holder):
         Item.__init__(self, holder, "WATER_BOTTLE", "Water Bottle")
+        self.consumed = False
+
+    def use(self):
+        if self.consumed:
+            print("The water bottle is empty. Tough!")
+            return
+
+        self.consumed = True
+        if self.holder.health > 100:
+            print("You just lost some HP. Idiot.")
+        elif self.holder.health == 100:
+            print("Nothing happened.")
+        else:
+            print("Health restored back to 100.")
+        self.holder.health = 100
+
